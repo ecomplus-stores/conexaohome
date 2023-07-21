b4e1287f8fab93dc9d63cf8163a9cd5bd48a091b
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-07-20 21:02:56 -0300

    Update storefront.webpack.js

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -3,8 +3,8 @@ module.exports = () => ({
   resolve: {
     alias: {
       './html/ProductCard.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductCard.html'),
-      './js/ProductCard.js': path.resolve(__dirname, 'template/js/custom-js/js/ProductCard.js'),
-      './base-config': path.resolve(__dirname, 'template/js/netlify-cms/base-config')
+      './js/ProductCard.js': path.resolve(__dirname, 'template/js/custom-js/js/ProductCard.js')
+      //'./base-config': path.resolve(__dirname, 'template/js/netlify-cms/base-config')
     }
   }
 })
