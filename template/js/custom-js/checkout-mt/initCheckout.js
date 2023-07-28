a61777f91e210749b9cb95d8615263aee2ff5ac0
Andersen Cypriano <acypriano@mtsolucoes.com.br>
2023-07-28 11:45:34 -0300

    revisao 0.1

--- template/js/custom-js/checkout-mt/initCheckout.js
+++ template/js/custom-js/checkout-mt/initCheckout.js
@@ -1,3 +1,4 @@
 export const initCheckout = () => {
   document.querySelector('body').classList.add('checkout-page')
+  console.log('add body class')
 }
--- template/js/custom-js/checkout.js
+++ template/js/custom-js/checkout.js
@@ -2,7 +2,11 @@ import { initCheckout } from "./checkout-mt/initCheckout";
 import { initCartPage } from "./mt-solucoes/cartPage";
 
 // Add your custom JavaScript for checkout here.
-window.location.hash.split('#/')[1] == 'checkout' ? initCheckout() : null;
+
+
+window.addEventListener("load", (event) => {
+  dataLayer.forEach(element => element.event == 'checkout' ? initCheckout() : null )
+});
 
 // CART PAGE
 window.location.hash.split('/')[1] == 'cart' ? initCartPage() : null;
