d3fa46b4526f528570bd7f22a5b1adebdb8110bd
Andersen Cypriano <acypriano@mtsolucoes.com.br>
2023-08-15 08:07:31 -0300

    teste: menu 2

--- template/js/netlify-cms/base-config/index.js
+++ template/js/netlify-cms/base-config/index.js
@@ -1,137 +1,135 @@
-import getSections from '@ecomplus/storefront-template/template/js/netlify-cms/base-config/sections'
-import getSettings from '@ecomplus/storefront-template/template/js/netlify-cms/base-config/collections/settings'
-import getLayout from '@ecomplus/storefront-template/template/js/netlify-cms/base-config/collections/layout'
-import getPages from '@ecomplus/storefront-template/template/js/netlify-cms/base-config/collections/pages'
-import getBlogPosts from '@ecomplus/storefront-template/template/js/netlify-cms/base-config/collections/blog-posts'
-import getExtraPages from '@ecomplus/storefront-template/template/js/netlify-cms/base-config/collections/extra-pages'
-import getWidgets from '@ecomplus/storefront-template/template/js/netlify-cms/base-config/collections/widgets'
+import getSections from "@ecomplus/storefront-template/template/js/netlify-cms/base-config/sections";
+import getSettings from "@ecomplus/storefront-template/template/js/netlify-cms/base-config/collections/settings";
+import getLayout from "@ecomplus/storefront-template/template/js/netlify-cms/base-config/collections/layout";
+import getPages from "@ecomplus/storefront-template/template/js/netlify-cms/base-config/collections/pages";
+import getBlogPosts from "@ecomplus/storefront-template/template/js/netlify-cms/base-config/collections/blog-posts";
+import getExtraPages from "@ecomplus/storefront-template/template/js/netlify-cms/base-config/collections/extra-pages";
+import getWidgets from "@ecomplus/storefront-template/template/js/netlify-cms/base-config/collections/widgets";
 
-export default options => {
+export default (options) => {
   options.sections = getSections(options).concat([
     {
-        label: "Grid de banners MT Soluções",
-        name: "banners-grid-mt",
-        widget: "object",
-        icon: "https://api.iconify.design/bi:grid.svg",
-        fields: [
+      label: "Grid de banners MT Soluções",
+      name: "banners-grid-mt",
+      widget: "object",
+      icon: "https://api.iconify.design/bi:grid.svg",
+      fields: [
         {
-            label: "Banners",
-            name: "banners",
-            widget: "list",
-            fields: [
+          label: "Banners",
+          name: "banners",
+          widget: "list",
+          fields: [
             {
-                label: "Imagem",
-                name: "img",
-                widget: "image"
+              label: "Imagem",
+              name: "img",
+              widget: "image",
             },
             {
-                label: "Link",
-                required: false,
-                name: "link",
-                widget: "string"
+              label: "Link",
+              required: false,
+              name: "link",
+              widget: "string",
             },
             {
-                label: "Título",
-                required: false,
-                name: "title",
-                widget: "string"
-            }
-            ]
-        }
-        ]
+              label: "Título",
+              required: false,
+              name: "title",
+              widget: "string",
+            },
+          ],
+        },
+      ],
     },
     {
-        label: "Instagram feed",
-        name: "instafeed",
-        widget: "object",
-        icon: "https://api.iconify.design/bi:grid.svg",
-        fields: [
-            {
-                label: "Ativar feed instagram",
-                name: "active",
-                widget: "boolean",
-                default: true
-            }
-        ]
+      label: "Instagram feed",
+      name: "instafeed",
+      widget: "object",
+      icon: "https://api.iconify.design/bi:grid.svg",
+      fields: [
+        {
+          label: "Ativar feed instagram",
+          name: "active",
+          widget: "boolean",
+          default: true,
+        },
+      ],
     },
     {
-        label: "Newsletter",
-        name: "newsletter-mt",
-        widget: "object",
-        icon: "https://api.iconify.design/bi:grid.svg",
-        fields: [
-            {
-                label: "Ativar newsletter",
-                name: "active",
-                widget: "boolean",
-                default: true
-            }
-        ]
-    }
-  ])
+      label: "Newsletter",
+      name: "newsletter-mt",
+      widget: "object",
+      icon: "https://api.iconify.design/bi:grid.svg",
+      fields: [
+        {
+          label: "Ativar newsletter",
+          name: "active",
+          widget: "boolean",
+          default: true,
+        },
+      ],
+    },
+  ]);
 
   return {
     backend: {
-      name: 'git-gateway',
-      branch: 'master',
+      name: "git-gateway",
+      branch: "master",
       commit_messages: {
-        create: 'Create {{collection}} “{{slug}}”',
-        update: 'Update {{collection}} “{{slug}}”',
-        delete: 'Delete {{collection}} “{{slug}}”',
-        uploadMedia: 'Upload “{{path}}”',
-        deleteMedia: '[skip ci] Delete “{{path}}”',
-        openAuthoring: '{{message}}'
-      }
+        create: "Create {{collection}} “{{slug}}”",
+        update: "Update {{collection}} “{{slug}}”",
+        delete: "Delete {{collection}} “{{slug}}”",
+        uploadMedia: "Upload “{{path}}”",
+        deleteMedia: "[skip ci] Delete “{{path}}”",
+        openAuthoring: "{{message}}",
+      },
     },
-    logo_url: 'https://ecom.nyc3.digitaloceanspaces.com/storefront/cms.png',
-    locale: 'pt',
+    logo_url: "https://ecom.nyc3.digitaloceanspaces.com/storefront/cms.png",
+    locale: "pt",
     load_config_file: Boolean(window.CMS_LOAD_CONFIG_FILE),
     media_folder: `${options.baseDir}template/public/img/uploads`,
-    public_folder: '/img/uploads',
+    public_folder: "/img/uploads",
     slug: {
-      encoding: 'ascii',
+      encoding: "ascii",
       clean_accents: true,
-      sanitize_replacement: '-'
+      sanitize_replacement: "-",
     },
     collections: [
       getSettings(options),
-      getLayout(options).files[0].fields.concat([
-        {
-          label: "Menu Secundario",
-          name: "menusecundario",
-          widget: "object",
-          fields: [
+      getLayout(options).files[0].fields.push({
+        label: "Menu Secundario",
+        name: "menusecundario",
+        widget: "object",
+        fields: [
           {
-              label: "Itens submenu",
-              name: "itensusbmenu",
-              widget: "list",
-              fields: [
+            label: "Itens submenu",
+            name: "itensusbmenu",
+            widget: "list",
+            fields: [
               {
-                  label: "Imagem",
-                  name: "img",
-                  widget: "image"
+                label: "Imagem",
+                name: "img",
+                widget: "image",
               },
               {
-                  label: "Link",
-                  required: false,
-                  name: "link",
-                  widget: "string"
+                label: "Link",
+                required: false,
+                name: "link",
+                widget: "string",
               },
               {
-                  label: "Título",
-                  required: false,
-                  name: "title",
-                  widget: "string"
-              }
-              ]
-          }
-          ]
-      }
-      ]),
+                label: "Título",
+                required: false,
+                name: "title",
+                widget: "string",
+              },
+            ],
+          },
+        ],
+      }),
       getPages(options),
       getBlogPosts(options),
       getExtraPages(options),
-      getWidgets(options)
-    ]
-  }
-}
+      getWidgets(options),
+    ],
+  };
+};
