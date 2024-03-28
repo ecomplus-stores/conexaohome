241794fdfae84d92f49e0e22439725b1a00a33fe
luizabrancaglion <92541373+luizabrancaglion@users.noreply.github.com>
2024-03-28 10:42:26 -0300

    h2 title page

--- template/js/custom-js/mt-solucoes/categoryPage/insertTitlePage.js
+++ template/js/custom-js/mt-solucoes/categoryPage/insertTitlePage.js
@@ -3,7 +3,7 @@ import { createElement } from "../utils/createElementHTML";
 export const insertTitlePage = () => {
   const title = document.title.split('|')[0];
   const contentBase = document.querySelector('.category-retail');
-  const elTitle = createElement('h1', 'title-page');
+  const elTitle = createElement('h2', 'title-page');
   const contentElTiltle = createElement('div', 'content-title-page');
   elTitle.textContent = title;
   contentElTiltle.appendChild(elTitle)
