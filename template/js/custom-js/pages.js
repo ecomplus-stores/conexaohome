957d2fda49a2cf4dd9af1a672cf96c40ab15b6c1
luizabrancaglion <92541373+luizabrancaglion@users.noreply.github.com>
2024-12-11 12:32:08 -0300

    Update pages.js
    [skip ci]

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -7,6 +7,16 @@ const insertArrowOnMenuMobile = () => {
     })
 }
 insertArrowOnMenuMobile();
-
-
-
+storefront.on('widget:@ecomplus/widget-minicart', function () {
+    document.querySelector('.minicart__btn-checkout').insertAdjacentHTML('beforebegin', `
+    <div id="block-confirm" class="form-group"><div class="custom-control custom-checkbox"><input type="checkbox" id="input-confirm-checkout" class="custom-control-input"> <label for="input-confirm-checkout" class="custom-control-label">
+          Eu li e aceito para continuar comprando
+        </label></div></div>
+    `);
+    document.querySelector('#block-confirm').addEventListener('click', (e) => {
+        if (e.target.checked) {
+            document.querySelector('#block-confirm').style.display = 'none'
+            document.querySelector('.minicart__btn-checkout').style.display = 'block'
+        }
+    })
+})
