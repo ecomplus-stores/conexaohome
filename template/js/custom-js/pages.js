5b4c309391e910c929de9ae18f40c6d9ccb0672c
luizabrancaglion <92541373+luizabrancaglion@users.noreply.github.com>
2024-12-19 10:23:17 -0300

    .

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -7,3 +7,17 @@ const insertArrowOnMenuMobile = () => {
     })
 }
 insertArrowOnMenuMobile();
+
+storefront.on('widget:@ecomplus/widget-minicart', function () {
+    document.querySelector('.minicart__btn-checkout').insertAdjacentHTML('beforebegin', `
+    <div id="block-confirm" class="form-group"><div class="custom-control custom-checkbox"><input type="checkbox" id="input-confirm-checkout" class="custom-control-input"> <label for="input-confirm-checkout" class="custom-control-label">
+          Ao finalizar a compra, confirmo que estou ciente e concordo que, devido ao recesso, os envios serão suspensos entre 20/12 e 06/01. O prazo de entrega começará a contar a partir de 06/01.
+        </label></div></div>
+    `);
+    document.querySelector('#block-confirm').addEventListener('click', (e) => {
+        if (e.target.checked) {
+            document.querySelector('#block-confirm').style.display = 'none'
+            document.querySelector('.minicart__btn-checkout').style.display = 'block'
+        }
+    })
+})
