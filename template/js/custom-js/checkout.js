217144872e16e9a5765c54aa4efb2bb7f0e017c9
luizabrancaglion <92541373+luizabrancaglion@users.noreply.github.com>
2025-12-18 17:47:35 -0300

    Update checkout.js

--- template/js/custom-js/checkout.js
+++ template/js/custom-js/checkout.js
@@ -10,17 +10,35 @@ window.addEventListener("load", (event) => {
 
 // CART PAGE
 window.location.hash.split('/')[1] == 'cart' ? initCartPage() : null;
+storefront.on('widget:@ecomplus/widget-minicart', function () {
+  const checkoutBtn = document.querySelector('.minicart__btn-checkout');
 
-storefront.on('widget:@ecomplus/widget-tag-manager', function () {
-    document.querySelector('.cart__btn-checkout').insertAdjacentHTML('beforebegin', `
-    <div id="block-confirm" class="form-group"><div class="custom-control custom-checkbox"><input type="checkbox" id="input-confirm-checkout" class="custom-control-input"> <label for="input-confirm-checkout" class="custom-control-label">
-          Ao finalizar a compra, confirmo que estou ciente e concordo que os pedidos realizados e aprovados entre 20/12 e 04/01 serão processados a partir do dia 05/01/2026, e o prazo de entrega passará a contar a partir dessa data.
-        </label></div></div>
-    `);
-    document.querySelector('#block-confirm').addEventListener('click', (e) => {
-        if (e.target.checked) {
-            document.querySelector('#block-confirm').style.display = 'none'
-            document.querySelector('.cart__btn-checkout').style.display = 'block'
-        }
-    })
-})
+  if (!checkoutBtn || document.querySelector('#block-confirm')) {
+    return;
+  }
+
+  // Esconde o botão de checkout inicialmente
+  checkoutBtn.style.display = 'none';
+
+  checkoutBtn.insertAdjacentHTML('beforebegin', `
+    <div id="block-confirm" class="form-group">
+      <div class="custom-control custom-checkbox">
+        <input type="checkbox" id="input-confirm-checkout" class="custom-control-input">
+        <label for="input-confirm-checkout" class="custom-control-label">
+          Ao finalizar a compra, confirmo que estou ciente e concordo que os pedidos realizados e aprovados 
+          entre 20/12 e 04/01 serão processados a partir do dia 05/01/2026, 
+          e o prazo de entrega passará a contar a partir dessa data.
+        </label>
+      </div>
+    </div>
+  `);
+
+  document
+    .querySelector('#input-confirm-checkout')
+    .addEventListener('change', function () {
+      if (this.checked) {
+        document.querySelector('#block-confirm').style.display = 'none';
+        checkoutBtn.style.display = 'block';
+      }
+    });
+});
