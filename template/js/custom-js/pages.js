232486c0c5960c259a97a6865dc944152a2f8e84
luizabrancaglion <92541373+luizabrancaglion@users.noreply.github.com>
2025-12-18 17:46:22 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -7,17 +7,35 @@ const insertArrowOnMenuMobile = () => {
     })
 }
 insertArrowOnMenuMobile();
-
 storefront.on('widget:@ecomplus/widget-minicart', function () {
-    document.querySelector('.minicart__btn-checkout').insertAdjacentHTML('beforebegin', `
-    <div id="block-confirm" class="form-group"><div class="custom-control custom-checkbox"><input type="checkbox" id="input-confirm-checkout" class="custom-control-input"> <label for="input-confirm-checkout" class="custom-control-label">
-          Ao finalizar a compra, confirmo que estou ciente e concordo que os pedidos realizados e aprovados entre 20/12 e 04/01 serão processados a partir do dia 05/01/2026, e o prazo de entrega passará a contar a partir dessa data.
-        </label></div></div>
-    `);
-    document.querySelector('#block-confirm').addEventListener('click', (e) => {
-        if (e.target.checked) {
-            document.querySelector('#block-confirm').style.display = 'none'
-            document.querySelector('.minicart__btn-checkout').style.display = 'block'
-        }
-    })
-})
+  const checkoutBtn = document.querySelector('.minicart__btn-checkout');
+
+  if (!checkoutBtn || document.querySelector('#block-confirm')) {
+    return;
+  }
+
+  // Esconde o botão de checkout inicialmente
+  checkoutBtn.style.display = 'none';
+
+  checkoutBtn.insertAdjacentHTML('beforebegin', `
+    <div id="block-confirm" class="form-group">
+      <div class="custom-control custom-checkbox">
+        <input type="checkbox" id="input-confirm-checkout" class="custom-control-input">
+        <label for="input-confirm-checkout" class="custom-control-label">
+          Ao finalizar a compra, confirmo que estou ciente e concordo que, devido ao recesso,
+          os envios serão suspensos entre 20/12 e 06/01.
+          O prazo de entrega começará a contar a partir de 06/01.
+        </label>
+      </div>
+    </div>
+  `);
+
+  document
+    .querySelector('#input-confirm-checkout')
+    .addEventListener('change', function () {
+      if (this.checked) {
+        document.querySelector('#block-confirm').style.display = 'none';
+        checkoutBtn.style.display = 'block';
+      }
+    });
+});
