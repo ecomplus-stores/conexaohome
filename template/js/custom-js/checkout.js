34934f9c7401d222aa788a8929d08b2e59b54208
luizabrancaglion <92541373+luizabrancaglion@users.noreply.github.com>
2025-12-18 17:52:44 -0300

    Update checkout.js

--- template/js/custom-js/checkout.js
+++ template/js/custom-js/checkout.js
@@ -10,35 +10,43 @@ window.addEventListener("load", (event) => {
 
 // CART PAGE
 window.location.hash.split('/')[1] == 'cart' ? initCartPage() : null;
-storefront.on('widget:@ecomplus/widget-minicart', function () {
-  const checkoutBtn = document.querySelector('.minicart__btn-checkout');
+window.addEventListener('load', function () {
+  function initCheckoutConfirm() {
+    const btnFinish = document.querySelector('.checkout__btn-confirm');
 
-  if (!checkoutBtn || document.querySelector('#block-confirm')) {
-    return;
+    if (!btnFinish || document.querySelector('#block-confirm')) {
+      return;
+    }
+
+    // Esconde o botão de finalizar pedido
+    btnFinish.style.display = 'none';
+
+    btnFinish.insertAdjacentHTML('beforebegin', `
+      <div id="block-confirm" class="form-group">
+        <div class="custom-control custom-checkbox">
+          <input type="checkbox" id="input-confirm-checkout" class="custom-control-input">
+          <label for="input-confirm-checkout" class="custom-control-label">
+            Ao finalizar a compra, confirmo que estou ciente e concordo que os pedidos realizados e aprovados<br>
+            entre 20/12 e 04/01 serão processados a partir do dia 05/01/2026,<br>
+            e o prazo de entrega passará a contar a partir dessa data.
+          </label>
+        </div>
+      </div>
+    `);
+
+    document
+      .querySelector('#input-confirm-checkout')
+      .addEventListener('change', function () {
+        if (this.checked) {
+          document.querySelector('#block-confirm').style.display = 'none';
+          btnFinish.style.display = 'block';
+        }
+      });
   }
 
-  // Esconde o botão de checkout inicialmente
-  checkoutBtn.style.display = 'none';
+  // Executa no momento certo do checkout
+  storefront.on('widget:@ecomplus/widget-tag-manager', initCheckoutConfirm);
 
-  checkoutBtn.insertAdjacentHTML('beforebegin', `
-    <div id="block-confirm" class="form-group">
-      <div class="custom-control custom-checkbox">
-        <input type="checkbox" id="input-confirm-checkout" class="custom-control-input">
-        <label for="input-confirm-checkout" class="custom-control-label">
-          Ao finalizar a compra, confirmo que estou ciente e concordo que os pedidos realizados e aprovados 
-          entre 20/12 e 04/01 serão processados a partir do dia 05/01/2026, 
-          e o prazo de entrega passará a contar a partir dessa data.
-        </label>
-      </div>
-    </div>
-  `);
-
-  document
-    .querySelector('#input-confirm-checkout')
-    .addEventListener('change', function () {
-      if (this.checked) {
-        document.querySelector('#block-confirm').style.display = 'none';
-        checkoutBtn.style.display = 'block';
-      }
-    });
+  // Fallback
+  setTimeout(initCheckoutConfirm, 1500);
 });
