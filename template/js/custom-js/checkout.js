3ee8bba6f548eeb88a270eb8e92c3e427805d081
luizabrancaglion <92541373+luizabrancaglion@users.noreply.github.com>
2025-12-18 18:09:08 -0300

    Update checkout.js

--- template/js/custom-js/checkout.js
+++ template/js/custom-js/checkout.js
@@ -11,41 +11,35 @@ window.addEventListener("load", (event) => {
 // CART PAGE
 window.location.hash.split('/')[1] == 'cart' ? initCartPage() : null;
 
-;(function () {
-  function addConfirmBlock() {
-    const btnFinish = document.querySelector('.checkout__btn-confirm');
-    if (!btnFinish || document.querySelector('#block-confirm')) return;
+storefront.on('widget:@ecomplus/widget-tag-manager', function () {
+  const checkoutBtn = document.querySelector('.cart__btn-checkout');
 
-    btnFinish.style.display = 'none';
-
-    btnFinish.insertAdjacentHTML('beforebegin', `
-      <div id="block-confirm" class="form-group">
-        <div class="custom-control custom-checkbox">
-          <input type="checkbox" id="input-confirm-checkout" class="custom-control-input">
-          <label for="input-confirm-checkout" class="custom-control-label">
-            Ao finalizar a compra, confirmo que estou ciente e concordo que os pedidos realizados e aprovados<br>
-            entre 20/12 e 04/01 serão processados a partir do dia 05/01/2026,<br>
-            e o prazo de entrega passará a contar a partir dessa data.
-          </label>
-        </div>
-      </div>
-    `);
-
-    document
-      .querySelector('#input-confirm-checkout')
-      .addEventListener('change', function () {
-        if (this.checked) {
-          document.querySelector('#block-confirm').remove();
-          btnFinish.style.display = 'block';
-        }
-      });
+  if (!checkoutBtn || document.querySelector('#block-confirm')) {
+    return;
   }
 
-  // Cria um observador que fica checando o DOM do checkout
-  const observer = new MutationObserver(() => addConfirmBlock());
-  observer.observe(document.body, {
-    childList: true,
-    subtree: true
-  });
-})();
+  // Esconde o botão inicialmente
+  checkoutBtn.style.display = 'none';
 
+  checkoutBtn.insertAdjacentHTML('beforebegin', `
+    <div id="block-confirm" class="form-group">
+      <div class="custom-control custom-checkbox">
+        <input type="checkbox" id="input-confirm-checkout" class="custom-control-input">
+        <label for="input-confirm-checkout" class="custom-control-label">
+          Ao finalizar a compra, confirmo que estou ciente e concordo que os pedidos realizados e aprovados<br>
+          entre 20/12 e 04/01 serão processados a partir do dia 05/01/2026,<br>
+          e o prazo de entrega passará a contar a partir dessa data.
+        </label>
+      </div>
+    </div>
+  `);
+
+  document
+    .querySelector('#input-confirm-checkout')
+    .addEventListener('change', function () {
+      if (this.checked) {
+        document.querySelector('#block-confirm').style.display = 'none';
+        checkoutBtn.style.display = 'block';
+      }
+    });
+});
