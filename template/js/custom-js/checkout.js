ffe15dceec955ed9d90a8840bf4cb16a729543b4
luizabrancaglion <92541373+luizabrancaglion@users.noreply.github.com>
2025-12-18 14:43:35 -0300

    aceite recesso checkout

--- template/js/custom-js/checkout.js
+++ template/js/custom-js/checkout.js
@@ -10,3 +10,47 @@ window.addEventListener("load", (event) => {
 
 // CART PAGE
 window.location.hash.split('/')[1] == 'cart' ? initCartPage() : null;
+
+window.addEventListener('load', () => {
+  if (typeof dataLayer !== 'undefined') {
+    dataLayer.forEach(element => {
+      if (element.event === 'checkout') {
+        initCheckout()
+      }
+    })
+  }
+})
+
+// CART PAGE
+if (window.location.hash.split('/')[1] === 'cart') {
+  initCartPage()
+}
+
+storefront.on('widget:@ecomplus/widget-tag-manager', function () {
+  const checkoutBtn = document.querySelector('.cart__btn-checkout')
+  if (!checkoutBtn) return
+
+  // esconde o botão inicialmente
+  checkoutBtn.style.display = 'none'
+
+  checkoutBtn.insertAdjacentHTML('beforebegin', `
+    <div id="block-confirm" class="form-group">
+      <div class="custom-control custom-checkbox">
+        <input type="checkbox" id="input-confirm-checkout" class="custom-control-input">
+        <label for="input-confirm-checkout" class="custom-control-label">
+          Ao finalizar a compra, confirmo que estou ciente e concordo que os pedidos realizados e aprovados entre 20/12 e 04/01 serão processados a partir do dia 05/01/2026, e o prazo de entrega passará a contar a partir dessa data.
+        </label>
+      </div>
+    </div>
+  `)
+
+  const confirmInput = document.querySelector('#input-confirm-checkout')
+
+  confirmInput.addEventListener('change', (e) => {
+    if (e.target.checked) {
+      document.querySelector('#block-confirm').style.display = 'none'
+      checkoutBtn.style.display = 'block'
+    }
+  })
+})
+
