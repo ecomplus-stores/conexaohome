36f3306c3d962b54ee8532c0aade3019d3e6bb06
luizabrancaglion <92541373+luizabrancaglion@users.noreply.github.com>
2025-12-19 10:55:11 -0300

    aceite checkout

--- template/js/custom-js/checkout.js
+++ template/js/custom-js/checkout.js
@@ -1,52 +1,58 @@
-import { initCheckout } from "./checkout-mt/initCheckout";
-import { initCartPage } from "./mt-solucoes/cartPage";
+import { initCheckout } from "./checkout-mt/initCheckout"
+import { initCartPage } from "./mt-solucoes/cartPage"
 
-// Add your custom JavaScript for checkout here.
-
+// CHECKOUT
 window.addEventListener("load", () => {
   dataLayer.forEach(element =>
     element.event === 'checkout' ? initCheckout() : null
-  );
-});
+  )
+
+  // AVISO DE RECESSO NO CHECKOUT
+  const interval = setInterval(() => {
+    const checkoutBtn = document.querySelector('.cart__btn-checkout')
+
+    // se ainda não existe, espera
+    if (!checkoutBtn) return
+
+    // se já tem o bloco, não duplica
+    if (document.querySelector('#block-confirm')) {
+      clearInterval(interval)
+      return
+    }
+
+    clearInterval(interval)
+
+    // esconde o botão de finalizar compra
+    checkoutBtn.style.display = 'none'
+
+    // insere o aviso antes do botão
+    checkoutBtn.insertAdjacentHTML('beforebegin', `
+      <div id="block-confirm" class="form-group">
+        <div class="custom-control custom-checkbox">
+          <input
+            type="checkbox"
+            id="input-confirm-checkout"
+            class="custom-control-input"
+          >
+          <label for="input-confirm-checkout" class="custom-control-label">
+            Ao finalizar a compra, confirmo que estou ciente e concordo que os pedidos realizados e aprovados
+            entre 20/12 e 04/01 serão processados a partir do dia 05/01/2026, e o prazo de entrega passará a contar a partir dessa data.
+          </label>
+        </div>
+      </div>
+    `)
+
+    const confirmInput = document.querySelector('#input-confirm-checkout')
+
+    // libera o botão somente após marcar o checkbox
+    confirmInput.addEventListener('change', function () {
+      if (this.checked) {
+        document.querySelector('#block-confirm').remove()
+        checkoutBtn.style.display = 'block'
+      }
+    })
+  }, 300)
+})
 
 // CART PAGE
-window.location.hash.split('/')[1] === 'cart' ? initCartPage() : null;
-
-// AVISO DE RECESSO NO CHECKOUT
-storefront.on('widget:@ecomplus/widget-tag-manager', function () {
-  const checkoutBtn = document.querySelector('.cart__btn-checkout');
-
-  // Se o botão ainda não existe ou se o aviso já foi inserido, não faz nada
-  if (!checkoutBtn || document.querySelector('#block-confirm')) {
-    return;
-  }
-
-  // Esconde o botão de finalizar compra
-  checkoutBtn.style.display = 'none';
-
-  // Insere o aviso antes do botão
-  checkoutBtn.insertAdjacentHTML('beforebegin', `
-    <div id="block-confirm" class="form-group">
-      <div class="custom-control custom-checkbox">
-        <input
-          type="checkbox"
-          id="input-confirm-checkout"
-          class="custom-control-input"
-        >
-        <label for="input-confirm-checkout" class="custom-control-label">
-          Ao finalizar a compra, confirmo que estou ciente e concordo que os pedidos realizados e aprovados entre 20/12 e 04/01 serão processados a partir do dia 05/01/2026, e o prazo de entrega passará a contar a partir dessa data.
-        </label>
-      </div>
-    </div>
-  `);
-
-  // Libera o botão somente após marcar o checkbox
-  document
-    .querySelector('#input-confirm-checkout')
-    .addEventListener('change', function () {
-      if (this.checked) {
-        document.querySelector('#block-confirm').style.display = 'none';
-        checkoutBtn.style.display = 'block';
-      }
-    });
-});
+window.location.hash.split('/')[1] === 'cart' ? initCartPage() : null
