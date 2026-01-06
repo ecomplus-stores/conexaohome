e09171723e70ef4d9ce4b5429870e9e526ec6106
luizabrancaglion <92541373+luizabrancaglion@users.noreply.github.com>
2026-01-06 12:17:15 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,10 +1,11 @@
 // Add your custom JavaScript for storefront pages here.
 const insertArrowOnMenuMobile = () => {
   document.querySelectorAll('#categories-nav a[data-toggle]').forEach(element => {
-    const contentArrow = document.createElement('div');
-    contentArrow.classList.add('arrow-menu');
-    element.appendChild(contentArrow);
-  });
-};
+    const contentArrow = document.createElement('div')
+    contentArrow.classList.add('arrow-menu')
+    element.appendChild(contentArrow)
+  })
+}
 
-insertArrowOnMenuMobile();
+storefront.on('widget:@ecomplus/widget-navbar', insertArrowOnMenuMobile)
+
